8bfa1793b2c47da8e9ec8cafed819e86fc82b069
SprintXXL <mspmain@pm.me>
2026-05-07 21:03:39 +0200

    Generate iron ore in the middle (ish) of 3x3 chunk regions

--- src/main/java/com/SprintXXL/primitiveoregen/worldgen/PrimitiveOreGenerator.java
+++ src/main/java/com/SprintXXL/primitiveoregen/worldgen/PrimitiveOreGenerator.java
@@ -1,5 +1,7 @@
 package com.SprintXXL.primitiveoregen.worldgen;
 
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.IChunkGenerator;
@@ -13,6 +15,15 @@ public class PrimitiveOreGenerator implements IWorldGenerator {
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
 
         if (Math.floorMod(chunkX, 3) == 1 && Math.floorMod(chunkZ, 3) == 1) {
+
+            int blockX = chunkX * 16 + 8;
+            int blockY = 40;
+            int blockZ = chunkZ * 16 + 8;
+
+            world.setBlockState(
+                    new BlockPos(blockX, blockY, blockZ),
+                    Blocks.IRON_ORE.getDefaultState()
+            );
             System.out.println("Ore Region Center Chunk: " + chunkX + ", " + chunkZ);
         }
     }
