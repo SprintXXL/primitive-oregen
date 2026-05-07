57eb75e64b3e052dff4c04029d9c433946f49bd0
SprintXXL <mspmain@pm.me>
2026-05-08 01:53:27 +0200

    First deposit + cell multiple layered ore vein prototype

--- src/main/java/com/SprintXXL/primitiveoregen/worldgen/PrimitiveOreGenerator.java
+++ src/main/java/com/SprintXXL/primitiveoregen/worldgen/PrimitiveOreGenerator.java
@@ -12,32 +12,58 @@ import java.util.Random;
 public class PrimitiveOreGenerator implements IWorldGenerator {
 
     @Override
-    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
-
+    public void generate(Random random, int chunkX, int chunkZ, World world,
+                         IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
         if (Math.floorMod(chunkX, 3) == 1 && Math.floorMod(chunkZ, 3) == 1) {
+            int centerX = chunkX * 16 + 8;
+            int centerY = 40;
+            int centerZ = chunkZ * 16 + 8;
 
-            int blockX = chunkX * 16 + 8;
-            int blockY = 40;
-            int blockZ = chunkZ * 16 + 8;
+            generateIronDeposit(world, random, centerX, centerY, centerZ);
+        }
+    }
 
-            for (int offsetX = -10; offsetX <= 10; offsetX++) {
-                for (int offsetY = -1; offsetY <= 2; offsetY++) {
-                    for (int offsetZ = -10; offsetZ <= 10; offsetZ++) {
+    public void generateIronDeposit(World world, Random random, int centerX, int centerY, int centerZ) {
 
-                        if (random.nextBoolean()) {
-                            world.setBlockState(
-                                    new BlockPos(blockX + offsetX, blockY + offsetY, blockZ + offsetZ),
-                                    Blocks.IRON_ORE.getDefaultState());
-                        }
-                        else {
-                            world.setBlockState(
-                                    new BlockPos(blockX + offsetX, blockY + offsetY, blockZ + offsetZ),
-                                    Blocks.STONE.getDefaultState()
-                            );
-                        }
-                    }
+        // Full deposit size
+        int radiusX = 9;
+        int radiusY = 4;
+        int radiusZ = 9;
+
+        // Cell Size
+        int cellSizeX = 3;
+        int cellSizeY = 3;
+        int cellSizeZ = 3;
+
+        for (int cellX = -radiusX; cellX <= radiusX; cellX += cellSizeX) {
+            for (int cellY = -radiusY; cellY <= radiusY; cellY += cellSizeY) {
+                for (int cellZ = -radiusZ; cellZ <= radiusZ; cellZ += cellSizeZ) {
+
+                    generateOreCell(world, random, centerX, centerY, centerZ, cellX, cellY, cellZ, cellSizeX, cellSizeY, cellSizeZ);
                 }
             }
+        }
+    }
+
+    public void generateOreCell(World world, Random random, int centerX, int centerY, int centerZ,
+                                int cellX, int cellY, int cellZ, int cellSizeX, int cellSizeY, int cellSizeZ) {
+
+        int oresPerCell = 2;
+
+        for (int i = 0; i < oresPerCell; i++) {
+
+
+            int localX = random.nextInt(cellSizeX);
+            int localY = random.nextInt(cellSizeY);
+            int localZ = random.nextInt(cellSizeZ);
+
+            BlockPos pos = new BlockPos(
+                    centerX + cellX + localX,
+                    centerY + cellY + localY,
+                    centerZ + cellZ + localZ
+            );
+
+            world.setBlockState(pos, Blocks.IRON_ORE.getDefaultState());
         }
     }
 }
